347d83ec99a4cfaf05a321f3d42c5d65def6d1d3
GhassenHammami <mohamedghassen.hammami@polytechnicien.tn>
2025-08-25 02:57:59 +0100

    Refactor routes to use string controller references

--- start/routes.ts
+++ start/routes.ts
@@ -9,28 +9,35 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
-const HomeController = () => import('#controllers/home_controller')
-const RegisterController = () => import('#controllers/auth/register_controller')
-const LoginController = () => import('#controllers/auth/login_controller')
-const LogoutController = () => import('#controllers/auth/logout_controller')
 
-router.get('/', [HomeController, 'index']).as('home').use(middleware.guest())
+router.get('/', '#controllers/home_controller.index').as('home').use(middleware.guest())
 
 router
   .group(() => {
     router
-      .get('/register', [RegisterController, 'show'])
+      .get('/register', '#controllers/auth/register_controller.show')
       .as('register.show')
       .use(middleware.guest())
+
     router
-      .post('/register', [RegisterController, 'store'])
+      .post('/register', '#controllers/auth/register_controller.store')
       .as('register.store')
       .use(middleware.guest())
 
-    router.get('/login', [LoginController, 'show']).as('login.show').use(middleware.guest())
-    router.post('/login', [LoginController, 'store']).as('login.store').use(middleware.guest())
+    router
+      .get('/login', '#controllers/auth/login_controller.show')
+      .as('login.show')
+      .use(middleware.guest())
 
-    router.post('/logout', [LogoutController, 'handle']).as('logout').use(middleware.auth())
+    router
+      .post('/login', '#controllers/auth/login_controller.store')
+      .as('login.store')
+      .use(middleware.guest())
+
+    router
+      .post('/logout', '#controllers/auth/logout_controller.handle')
+      .as('logout')
+      .use(middleware.auth())
   })
   .prefix('/auth')
   .as('auth')
